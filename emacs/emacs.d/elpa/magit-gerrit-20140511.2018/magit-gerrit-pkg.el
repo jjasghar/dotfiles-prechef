e8eb4a694394dd98695c25fe3ee7dacc6f3e3cc6
JJ Asghar <jj@getchef.com>
2014-07-30 14:42:59 -0500

    added my emacs.d directory and updated my .emacs

---
+++ emacs/emacs.d/elpa/magit-gerrit-20140511.2018/magit-gerrit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "magit-gerrit" "20140511.2018" "Magit plugin for Gerrit Code Review" (quote ((magit "1.2.0"))))
---
+++ emacs/emacs.d/elpa/ruby-test-mode-20140727.204/ruby-test-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ruby-test-mode" "20140727.204" "Minor mode for Behaviour and Test Driven" (quote ((ruby-mode "1.0"))))
