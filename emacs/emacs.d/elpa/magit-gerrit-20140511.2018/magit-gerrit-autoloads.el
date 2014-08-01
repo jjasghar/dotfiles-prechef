4ee7110e4601056e47cf843698949f3ce048ce32
JJ Asghar <jj@getchef.com>
2014-08-01 16:45:38 -0500

    removed emacs.d

--- emacs/emacs.d/elpa/magit-gerrit-20140511.2018/magit-gerrit-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; magit-gerrit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("magit-gerrit-pkg.el" "magit-gerrit.el")
-;;;;;;  (21463 63887 511930 0))
-
-;;;***
-
-(provide 'magit-gerrit-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; magit-gerrit-autoloads.el ends here
--- emacs/emacs.d/elpa/ruby-test-mode-20140727.204/ruby-test-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ruby-test-mode" "20140727.204" "Minor mode for Behaviour and Test Driven" (quote ((ruby-mode "1.0"))))
